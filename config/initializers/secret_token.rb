c808d99e16c704fa2c7af8103ade6918fbeb5430
Chris Tolin <chris.tolin@clickbank.com>
2013-09-28 13:14:59 -0600

    Baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'db86a66ecae31ef38d68afb20f0a53e83ff0fc925f08a46fd929f793f77b22963ab130a139b17e41b90629a5487560d651a5b560b4f6fc000dfe44186088ec86'
